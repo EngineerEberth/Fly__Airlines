151a16200f89ec3eb7e10605ab663546b6993a70
EngineerEberth <n7204845@gmail.com>
2024-12-31 19:35:46 -0500

    ++++

--- src/middlewares/valideToken.js
+++ src/middlewares/valideToken.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken'
-import { TOKEN_SECRET } from '../config.js';
+import { TOKEN_SECRET } from '../../config.js';
 export const authRequired=(req,res,next)=>{
     const{token}=req.cookies
     if(!token) return res.status(401).json({message:"No token, authorization denied"})
